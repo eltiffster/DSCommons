3d7aaa4c8e65ee79e05275b00fddb1ed55a62283
Tiffany Chan <tiff.chan29@gmail.com>
2018-02-21 10:13:51 -0800

    tilt kaleidoscope

---
+++ tiltKalColour.ino
@@ -0,0 +1,193 @@
+/*
+  Tilt Kaleidoscope with Colour
+  By Tiffany Chan (http://thisistiffanychan.com/), 
+  based on notes by Lucas Ainsworth (https://goo.gl/SpcBwB)
+
+  Reads the input of a tilt sensor and changes the colour of an LED strip accordingly. 
+  This requires the NeoPixel Library.
+  
+  This code is licensed under an Attribution-NonCommercial-ShareAlike 4.0 
+  International License (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
+*/
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+//These are instructions and settings for the NeoPixel library.
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(36, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+int inPin = 2;         // the number of the input pin (tilt sensor)
+int outPin = 6;       // the number of the output pin (LED strip)
+int reading;           // the current reading from the input pin
+int previous = LOW;    // the previous reading from the input pin
+
+//Because the tilt sensor is very sensitive, it might accidentally misfire when
+//we don't want it to. To prevent this, we do what's called "debouncing" or
+//stabilizing the value.
+// the following variables are long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long time = 0;         // the last time the output pin was toggled
+long debounce = 50;   // the debounce time, increase if the output flickers
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(9600); //Start the serial monitor (good for debugging)
+}
+
+void loop() {
+
+  int switchstate;
+ 
+  reading = digitalRead(inPin);
+  Serial.println(reading);
+ 
+  // If the switch changed, due to bounce or pressing...
+  if (reading != previous) {
+    // reset the debouncing timer
+    time = millis();
+  } 
+ 
+  if ((millis() - time) > debounce) {
+     // whatever the switch is at, its been there for a long time
+     // so lets settle on it!
+     switchstate = reading;
+     
+     // Now invert the output on the LED strip
+    if (switchstate == LOW)
+    colorWipe(strip.Color(0, 0, 255), 50); // Blue; you can change this if you want
+    else
+    colorWipe(strip.Color(255, 0, 0), 50); // Red; you can change this if you want
+  }
+ 
+  // Save the last reading so we keep a running tally
+  previous = reading;
+
+// Below is a list of functions (i.e. some action or 
+// animation Arduino can do with the LED strip. To call a function, copy
+//the code and paste it above under the "if" or "else" line. To find out
+//what it does, scroll down for a description or refer to the handout
+//...or just uncomment (delete the //) the code in and see what happens!
+
+//  colorWipe(strip.Color(255, 0, 0), 50); // Red
+//  colorWipe(strip.Color(0, 255, 0), 50); // Green
+//  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+
+//  theaterChase(strip.Color(127, 127, 127), 50); // White
+//  theaterChase(strip.Color(127, 0, 0), 50); // Red
+//  theaterChase(strip.Color(0, 0, 127), 50); // Blue
+//
+//  rainbow(20);
+//  rainbowCycle(20);
+//  theaterChaseRainbow(50);
+}
+
+//These are the longer descriptions or instructions Arduino is following
+//(in programming, they are called "functions"). The lines above are a 
+//shorthand way to "call" or trigger one or more of the functions 
+//defined below. You don't really need to touch the code beyond this point
+//but if you're interested, it's good to see what's under the hood.
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
